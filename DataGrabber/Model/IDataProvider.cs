53164b9fd444e629994d535a560230cf78493ecd
analog <logachev14@gmail.com>
2017-08-11 21:48:08 +0300

    немного xaml'a, провайдер

---
+++ DataGrabber/Model/IDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataGrabber.Model
+{
+    interface IDataProvider
+    {
+        bool Init(string port, int baudRate);
+        event EventHandler<Data> DataLenghtReceived;
+        event EventHandler<Data> DataPeriodReceived;
+    }
+    public class Data : EventArgs
+    {
+        public int data;
+    }
+    
+
+}
---
+++ DataGrabber/Model/SerialPortProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Threading;
+
+namespace DataGrabber.Model
+{
+    class SerialPortProvider : IDataProvider
+    {
+        public SerialPortProvider()
+        {
+            _invoker = Invoke;
+        }
+
+
+        SerialPort _port;
+        Thread _listener;
+        delegate void Invoker();
+        Invoker _invoker;
+        public event EventHandler<Data> DataLenghtReceived;
+        public event EventHandler<Data> DataPeriodReceived;
+
+        public bool Init(string port, int baudRate)
+        {
+            try
+            {
+                _port = new SerialPort(port, baudRate);
+                _port.Open();
+                _listener = new Thread(Listen) { IsBackground = true };
+                _listener.Start();
+            }
+            catch(Exception e)
+            {
+                Trace.WriteLine(e);
+                return false;
+            }
+            return _port.IsOpen;
+        }
+
+        private void Listen()
+        {
+            while(true)
+            {
+                App.Current.Dispatcher.BeginInvoke(_invoker);
+            }
+        }
+        private void Invoke()
+        {
+
+        }
+    }
+}
